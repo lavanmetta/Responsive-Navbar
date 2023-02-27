27159597d10291ce4d6b0ee19cc7d840c6e2548a
lavanmetta <lavanmetta111@gmail.com>
2023-02-28 00:20:24 +0530

    nav

---
+++ app.js
@@ -0,0 +1,19 @@
+const hamBurgerBtn = document.getElementById('btn');
+const  links = document.getElementById('links');
+
+
+
+
+
+
+
+hamBurgerBtn.addEventListener('click', function() {
+
+    links.classList.toggle('mobile')
+   
+    hamBurgerBtn.classList.toggle("ri-menu-unfold-fill")
+
+    links.classList.toggle('animation')
+
+    links.classList.add('animation')
+})
